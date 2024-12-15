1a28fac027055890b5fe1e50d4a58185334510fe
Igor Bogdanovic <30838760+IgorBogdanovic@users.noreply.github.com>
2024-12-15 17:56:53 +0100

    chore: Add @ alias to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { URL, fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
 });
