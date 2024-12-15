98a278fa07153bb32b88bce91ca8d8a76f7a58c6
Igor Bogdanovic <30838760+IgorBogdanovic@users.noreply.github.com>
2024-12-15 17:55:42 +0100

    chore: Set eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,29 @@
-import pluginVue from 'eslint-plugin-vue';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended.js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginVue from 'eslint-plugin-vue';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
 
 export default [
   ...pluginVue.configs['flat/essential'],
   ...pluginVue.configs['flat/strongly-recommended'],
   ...pluginVue.configs['flat/recommended'],
+  ...vueTsEslintConfig(),
   eslintPluginPrettierRecommended,
   eslintConfigPrettier,
   {
     rules: {
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: false,
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
       'prettier/prettier': [
         'error',
         {
